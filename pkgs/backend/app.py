08ce3d42894e06484b2f724d6271ca4dc111e370
mashharuki <avp.104.106.107@ae.auone-net.jp>
2024-07-23 23:00:35 +0900

    chore: Add opencv-python, scikit-image, numpy, torch, torchvision, torchaudio, Pillow, flask, and flask-cors to backend requirements.txt

---
+++ pkgs/backend/app.py
@@ -0,0 +1,81 @@
+from flask import Flask, request, jsonify
+import cv2
+import numpy as np
+from skimage.filters import gaussian
+from test import evaluate
+import base64
+import json
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # ここでCORSを設定
+
+def sharpen(img):
+    img = img * 1.0
+    gauss_out = gaussian(img, sigma=5, channel_axis=-1)
+
+    alpha = 1.5
+    img_out = (img - gauss_out) * alpha + img
+
+    img_out = img_out / 255.0
+
+    mask_1 = img_out < 0
+    mask_2 = img_out > 1
+
+    img_out = img_out * (1 - mask_1)
+    img_out = img_out * (1 - mask_2) + mask_2
+    img_out = np.clip(img_out, 0, 1)
+    img_out = img_out * 255
+    return np.array(img_out, dtype=np.uint8)
+
+def change_hair_color(image, parsing, part=17, color=[255, 215, 0]):
+    b, g, r = color
+    tar_color = np.zeros_like(image)
+    tar_color[:, :, 0] = b
+    tar_color[:, :, 1] = g
+    tar_color[:, :, 2] = r
+
+    image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    tar_hsv = cv2.cvtColor(tar_color, cv2.COLOR_BGR2HSV)
+
+    if part == 12 or part == 13:
+        image_hsv[:, :, 0:2] = tar_hsv[:, :, 0:2]
+    else:
+        image_hsv[:, :, 0:1] = tar_hsv[:, :, 0:1]
+
+    changed = cv2.cvtColor(image_hsv, cv2.COLOR_HSV2BGR)
+
+    if part == 17:
+        changed = sharpen(changed)
+
+    changed[parsing != part] = image[parsing != part]
+    return changed
+
+@app.route('/change_hair_color', methods=['POST'])
+def process_image():
+    if 'image' not in request.files:
+        return jsonify({"error": "No image file provided"}), 400
+
+    image_file = request.files['image']
+    image_path = 'uploaded_image.jpg'
+    image_file.save(image_path)
+
+    color_str = request.form.get('color', '[255, 215, 0]')
+    color = json.loads(color_str)
+
+    cp = 'cp/79999_iter.pth'
+
+    image = cv2.imread(image_path)
+    ori = image.copy()
+    parsing = evaluate(image_path, cp)
+    parsing = cv2.resize(parsing, image.shape[0:2], interpolation=cv2.INTER_NEAREST)
+
+    image = change_hair_color(image, parsing, part=17, color=color)
+
+    _, buffer = cv2.imencode('.jpg', image)
+    encoded_image = base64.b64encode(buffer).decode('utf-8')
+    
+    return jsonify(encoded_image)
+
+if __name__ == '__main__':
+    app.run(debug=True)
